9080136d60dec31e2172aee653d754eff115200f
Jono Shields <jonathon.shields@vodafone.com>
2016-11-30 13:05:41 +1300

    added fog and speckle

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var ctx;
 var canvas;
-var pixelSize = 6;
+var pixelSize = 8;
 
 var rule = 62;
 var ruleArray = new Array(8);
@@ -57,11 +57,17 @@ function perform(){
             }
         }
     }
+
+    drawRect(0,0, '#2f5d87', canvas.width, canvas.height);
+    drawPixelFog(1);
+    drawSun('#F9E79F', (0.5*width*Math.random() + 0.2*width)*pixelSize,Math.random()*0.2*height*pixelSize,Math.random() * 0.1*width*pixelSize);
+   
     
     for(var y = 0; y< height; y++){
         for(var x = 0; x< width; x++){
             if(image[y][x]){
-                drawSquare(x*pixelSize,y*pixelSize, 'white', pixelSize);
+                var whiteColor = Math.floor(255-Math.random()*10);
+                drawSquare(x*pixelSize,y*pixelSize, 'rgb('+whiteColor+','+whiteColor+','+whiteColor+')', pixelSize);
                 //Caves
                 if(image[y][x-3] && image[y][x-2] && image[y][x-1]&& image[y][x+1]&& image[y][x+2]&& image[y][x+3]&&Math.random()<0.03){
                     drawSquare(x*pixelSize,(y-1)*pixelSize, 'black', pixelSize);
@@ -82,33 +88,37 @@ function perform(){
                 }
                 //Draw Tree
                 if(Math.random()<0.0001*y ){
+                    var speckledGreen = Math.floor(130-Math.random()*60);
+                    var green = 'rgb('+50+','+speckledGreen+','+50+')';
                     drawSquare(x*pixelSize,(y-1)*pixelSize, '#422c10', pixelSize);
-                    drawSquare(x*pixelSize,(y-2)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare((x+1)*pixelSize,(y-2)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare((x-1)*pixelSize,(y-2)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare((x+1)*pixelSize,(y-3)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare((x-1)*pixelSize,(y-3)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare(x*pixelSize,(y-3)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare(x*pixelSize,(y-4)*pixelSize, '#1a4a3c', pixelSize);
-                    drawSquare(x*pixelSize,(y-5)*pixelSize, '#1a4a3c', pixelSize);
+//                    drawSquare(x*pixelSize,(y-2)*pixelSize, '#1a4a3c', pixelSize);
+                    drawSquare(x*pixelSize,(y-2)*pixelSize, green, pixelSize);
+                    drawSquare((x+1)*pixelSize,(y-2)*pixelSize, green, pixelSize);
+                    drawSquare((x-1)*pixelSize,(y-2)*pixelSize, green, pixelSize);
+                    drawSquare((x+1)*pixelSize,(y-3)*pixelSize, green, pixelSize);
+                    drawSquare((x-1)*pixelSize,(y-3)*pixelSize, green, pixelSize);
+                    drawSquare(x*pixelSize,(y-3)*pixelSize, green, pixelSize);
+                    drawSquare(x*pixelSize,(y-4)*pixelSize, green, pixelSize);
+                    drawSquare(x*pixelSize,(y-5)*pixelSize, green, pixelSize);
                 }
                 //Draw Rock
                 if(y>1 && !image[y-1][x] && Math.random()<0.001*y ){
                     drawSquare(x*pixelSize,(y-1)*pixelSize, 'silver', pixelSize);
                 }
             } else {
-                drawSquare(x*pixelSize,y*pixelSize, '#2f5d87', pixelSize);
+//                drawSquare(x*pixelSize,y*pixelSize, '#2f5d87', pixelSize);
                 var tmp = y;
                 while(tmp>0){
+                    var speckledGrey = Math.floor(220-Math.random()*30);
                     if(image[tmp][x]){
-                        drawSquare(x*pixelSize,y*pixelSize, 'lightgray', pixelSize);
+                        drawSquare(x*pixelSize,y*pixelSize, 'rgb('+speckledGrey+','+speckledGrey+','+speckledGrey+')', pixelSize);
                         tmp=0;
                     }
                     tmp--;
                 }
                 if(y>1 && image[y-1][x]){
                     if(y>1 && image[y-2][x]){
-                        drawSquare(x*pixelSize,y*pixelSize, 'grey)', pixelSize);
+                        drawSquare(x*pixelSize,y*pixelSize, 'grey', pixelSize);
                     } else {
                         drawSquare(x*pixelSize,y*pixelSize, 'silver', pixelSize);
                     }
@@ -124,21 +134,23 @@ function perform(){
     for(var y = 4; y< height-1; y++){
         for(var x = 1; x< width-1; x++){
             if(Math.random()<0.00005*y*pixelSize){
-                if(waterfallNum<5){
+                if(waterfallNum<y/30){
                     if(!image[y][x] && !image[y][x-1]&& !image[y][x+1] && 
                        !image[y-1][x] && !image[y-1][x-1]&& !image[y-1][x+1] && 
                        !image[y-2][x] && !image[y-2][x-1]&& !image[y-2][x+1] && 
                        !image[y-3][x] && !image[y-3][x-1]&& !image[y-3][x+1] && 
                        image[y-4][x] && image[y-4][x-1]&& image[y-4][x+1]){
-                        drawSquare(x*pixelSize,y*pixelSize, 'dimgrey', pixelSize);
+                        drawSquare(x*pixelSize,y*pixelSize, 'black', pixelSize);
                         waterfallNum++;
                         for(var i = 1; i < height-y; i++){
+                            var speckledOther = Math.floor(130-Math.random()*40);
+                            var speckledBlue = Math.floor(200-Math.random()*40);
                             if(!image[y+i][x]){
-                                drawSquare(x*pixelSize,(y+i)*pixelSize, 'skyblue', pixelSize);
+                                drawSquare(x*pixelSize,(y+i)*pixelSize, 'rgb('+(speckledOther-10)+','+speckledOther+','+speckledBlue+')', pixelSize);
                             } else if(image[y+i][x-1]){
-                                drawSquare((x-1)*pixelSize,(y+i)*pixelSize, 'skyblue', pixelSize);
+                                drawSquare((x-1)*pixelSize,(y+i)*pixelSize, 'rgb('+(speckledOther-10)+','+speckledOther+','+speckledBlue+')', pixelSize);
                             } else if(image[y+i][x+1]){
-                                drawSquare((x+1)*pixelSize,(y+i)*pixelSize, 'skyblue', pixelSize);
+                                drawSquare((x+1)*pixelSize,(y+i)*pixelSize, 'rgb('+(speckledOther-10)+','+speckledOther+','+speckledBlue+')', pixelSize);
                             }
                         }
                     }
@@ -147,6 +159,22 @@ function perform(){
             }
         }
     }
+    drawPixelFog(0.3);
+}
+
+function drawPixelFog(percentage){
+    for(var y = height; y > 0; y--){
+        for(var x = 0; x < width; x++){
+            ctx.globalAlpha=y/height*percentage;
+            drawSquare(x*pixelSize,y*pixelSize, 'white', pixelSize);
+        }
+    }
+    ctx.globalAlpha=1;
+}
+
+function drawSun(colour, x,y,r){ 
+    drawRect(x-r/2+pixelSize/2,y-r/2+r/10+pixelSize/2,colour,r, r-0.2*r);
+    drawRect(x-r/2+r/10+pixelSize/2,y-r/2+pixelSize/2,colour,r-0.2*r, r);
 }
 
 function calculateCell(a){
@@ -200,6 +228,11 @@ function drawPixel(x,y,color){
 function drawSquare(x,y,color,size){
     ctx.fillStyle =color;
     ctx.fillRect(x,y,size,size);
+}
+
+function drawRect(x,y,color,width, height){
+    ctx.fillStyle =color;
+    ctx.fillRect(x,y,width,height);
 }
 
 function resizeCanvas(e) {
